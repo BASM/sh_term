305a02c0806b6e79ff97f9e1eb1ac92d9276dd1e
Leonid Myravjev <asm@asm.pp.ru>
2013-10-07 00:27:54 +0400

    Worked variant this libstwi (Lib Software TWI)

--- firmware/env.sh
+++ firmware/env.sh
@@ -1,3 +1,4 @@
 #export DEVICE=attiny45
+export CC=avr-gcc
 export DEVICE=atmega328p
 export FREQ=12000000UL
--- firmware/main.c
+++ firmware/main.c
@@ -6,15 +6,20 @@
 #include <uart.h>
 #include <twi.h>
 #include <adc.h>
+#include <twi_sw.h>
 
 #define MAXTERM_PREF 0x4f
-#define DSTERM_PREF 0x4d
+#define DSTERM_PREF   0x4d
+#define DSTERMSW_PREF 0x4f
 #define HUMI_CHAN 0x07
 
 static int getcurterm() {
   int res;
   
-  twi_req_read(MAXTERM_PREF,0x00);
+  if (twi_req_read(MAXTERM_PREF,0x00)) {
+    twi_p_stop();
+    return -1;
+  }
   res=twi_p_read(0)<<8;
   res|=twi_p_read(1);
   twi_p_stop();
@@ -22,6 +27,29 @@ static int getcurterm() {
   //res=((res&0x3ffff)>>3)*0.0625*10;// 0x3ffff --remove sign, >>3-- remove LSB bit, 0.0625 -- 1LSB, 10 -- output at 0.1C
   res=((res&0x3ffff)>>3)*0.0625*10;// 0x3ffff --remove sign, >>3-- remove LSB bit, 0.0625 -- 1LSB,
   return res;
+}
+
+static int getcurtermSDS(twi_data *twi) {
+  int res;
+  
+  printf("RR\n");
+  if (twi_sw_req_read(twi,DSTERMSW_PREF,0xAA)) {
+    return 0;
+  }
+  res =twi_sw_read(twi,0)<<8;
+  res|=twi_sw_read(twi,1);
+  twi_sw_stop(twi);
+
+  //res=((res&0x3ffff)>>3)*0.0625*10;// 0x3ffff --remove sign, >>3-- remove LSB bit, 0.0625 -- 1LSB, 10 -- output at 0.1C
+  res=((res&0x3ffff)>>3)*0.03125*10;// 0x3ffff --remove sign, >>3-- remove LSB bit, 0.0625 -- 1LSB,
+  
+  twi_sw_req_read(twi,DSTERMSW_PREF,0xEE);
+  twi_sw_stop(twi);
+  return res;
+
+
+
+  return 0;
 }
 
 static int getcurtermDS() {
@@ -40,10 +68,58 @@ static int getcurtermDS() {
   return res;
 }
 
+static void s_cycle_wait(void *data) {
+  _delay_us(100);
+}
+
+#define SCL_NUM  0
+#define SCL_DDR  DDRC
+#define SCL_PORT PORTC
+#define SCL_PIN  PINC
+
+#define SDA_NUM  1
+#define SDA_DDR  DDRC
+#define SDA_PORT PORTC
+#define SDA_PIN  PINC
+
+static int s_scl_rl(void *data) {
+  //PC0 
+  SCL_DDR &=~(1<<SCL_NUM);
+
+  //while (((SCL_PIN>>SCL_NUM)&0x01)==0) {
+  //  printf("W");
+  //}
+  return 0;
+}
+
+static void s_scl_dn(void *data) {
+  SCL_DDR |= 1<<SCL_NUM;
+}
+
+static void s_sda_rl(void *data) {
+  SDA_DDR &=~(1<<SDA_NUM);
+}
+
+static void s_sda_dn(void *data) {
+  SDA_DDR |= (1<<SDA_NUM);
+}
+
+static int s_sda_read(void *data) {
+
+  return !((SDA_PIN>>SDA_NUM)&0x01);
+}
+
+static int s_scl_read(void *data) {
+
+  return !((SCL_PIN>>SCL_NUM)&0x01);
+}
+
+
 int main(void) {
   int term;
   int term1;
-  int adc;
+  int term2;
+  //int adc;
   unsigned int i;
 
   cli();
@@ -55,6 +131,23 @@ int main(void) {
 
   printf("HEllo!!\n");
 
+  twi_data stwi_obj;
+  twi_data *stwi=&stwi_obj;
+
+  //////////////////////////////
+  /////// SOFTWARE TWI /////////
+  SCL_PORT &=~(1<<SCL_NUM);//
+  SDA_PORT &=~(1<<SDA_NUM);//
+  stwi->scl_rl=s_scl_rl;
+  stwi->scl_dn=s_scl_dn;
+  stwi->sda_rl=s_sda_rl;
+  stwi->sda_dn=s_sda_dn;
+  stwi->sda_read=s_sda_read;
+  stwi->scl_read=s_scl_read;
+  stwi->cycle_wait=s_cycle_wait;
+  twi_sw_init(stwi,NULL);
+  //////////////////////////////
+
 
   DDRC=0x00;
   DDRB=0xff;
@@ -64,10 +157,12 @@ int main(void) {
 
   //OC1A -- PB1
 
+  printf("HELLO!\n");
   i=0;
   while (1) {
     i++;
 
+    printf("CYCLE START! %i\n", i);
     PORTB=0;
     //PORTD=0;
     _delay_ms(50);
@@ -75,9 +170,14 @@ int main(void) {
     PORTD=1<<3;
     _delay_ms(2000);
 
+    printf("CURRENT 1\n");
     term=getcurterm();
+    printf("CURRENT 2\n");
     term1=getcurtermDS();
-    printf("Current term is: %i, and %i\n", term, term1);
+    
+    printf("CURRENT 3\n");
+    term2=getcurtermSDS(stwi);
+    printf("Current term is: term %i, term1 %i, swterm: %i\n", term, term1, term2);
   }
 
 
